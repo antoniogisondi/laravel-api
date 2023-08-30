d084706711e5473a42405b36d90aa01e4a512936
antoniogisondi <anto.gisondi@tiscali.it>
2023-08-30 10:06:04 +0200

    Fixed bonus

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -12,7 +12,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with('type', 'technologies')->paginate(4);
+        $projects = Project::with('type', 'technologies')->paginate(2);
         return response()->json([
             'success' => true,
             'results' => $projects
