cf686e1651b104ccf0fe4d36fc34546706323924
antoniogisondi <anto.gisondi@tiscali.it>
2023-08-30 09:19:38 +0200

    Modify api (continue on vite-boolfolio repository)

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Project;
+use App\Models\Type;
+use App\Models\Technology;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::all();
+        $projects = Project::with('type', 'technologies')->get();
         return response()->json([
             'success' => true,
             'results' => $projects
